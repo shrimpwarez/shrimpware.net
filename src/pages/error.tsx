ea3c1cfa15cd04a6a5d26f0b587b022cadd3c729
nyaruku <38096703+nyaruku@users.noreply.github.com>
2026-09-15 00:10:51 +0200

    Show shrimp on error page

--- src/pages/error.tsx
+++ src/pages/error.tsx
@@ -1,12 +1,12 @@
+import shrimple from '../img/shrimple.svg';
+
 export default function Error() {
     return (
         <>
             <title>Error</title>
             <div className="container d-flex justify-content-center">
-                <div className="card card-error mt-5">
-                    <div className="card-body">
-                        <p className="card-text">ERROR</p>
-                    </div>
+                <div className="inner-container text-center">
+                    <img className="img-fluid" src={shrimple} alt="shrimple" />
                 </div>
             </div>
         </>
